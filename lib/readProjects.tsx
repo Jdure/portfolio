cae9ac4f6424233b3d2a18130619653de34550fa
Jdure <22172148+Jdure@users.noreply.github.com>
2023-02-14 10:03:11 -0500

    added helper function

---
+++ lib/readProjects.tsx
@@ -0,0 +1,28 @@
+import fs from "fs"
+import path from "path"
+import matter from "gray-matter"
+
+const projectsDir = path.join(process.cwd(), "projects")
+
+export const getAllProjectSlugs = () => {
+    const fileNames = fs.readdirSync(projectsDir)
+    return fileNames.map(fileName => {
+        return {
+            params: {
+                slug: filename.split(".")[0]
+            }
+        }
+    })
+}
+
+export function getProjectData(slug:string) {
+    const fullPath = path.join(projectsDir, `${slug}.md`)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+    const matterResult = matter(fileContents)
+
+    return {
+        id, 
+        ...matterResult.data
+    }
+}

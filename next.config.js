066286e43795c800d895478548b2559e4c842530
Jdure <22172148+Jdure@users.noreply.github.com>
2023-02-26 23:09:01 -0500

    removed i18n

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   images: {
     domains: ["source.unsplash.com", "cdn.jsdelivr.net", "res.cloudinary.com"],
   },
-  i18n: {
-    locales: ["en-CA", "fr-CA"],
-    defaultLocale: "en-CA",
-  },
   output: "standalone",
 };
 
